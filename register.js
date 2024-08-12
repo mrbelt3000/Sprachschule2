cfc2d3807602a52b95f695111a0b0c0061151b69
mrbelt3000 <148152346+mrbelt3000@users.noreply.github.com>
2024-08-12 14:02:44 +0200

    Update register.js

--- register.js
+++ register.js
@@ -5,6 +5,10 @@
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
